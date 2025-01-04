4fd4b7c0aded5acb6772402def8e4dd4fcca497a
notshrirang <shrirangmahajan123@gmail.com>
2025-01-04 17:26:00 +0530

    feat: add audio data input

--- vectordb.py
+++ vectordb.py
@@ -11,14 +11,17 @@ from sentence_transformers import SentenceTransformer
 import streamlit as st
 import torch
 import time
+import whisper
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 os.makedirs("./vectorstore", exist_ok=True)
 
-def update_vectordb(index_path: str, embedding: torch.Tensor, image_path: str = None, text_content: str = None):
+def update_vectordb(index_path: str, embedding: torch.Tensor, image_path: str = None, text_content: str = None, audio_path: str = None):
     if not image_path and not text_content:
         raise ValueError("Either image_path or text_content must be provided.")
+    if audio_path and not text_content:
+        raise ValueError("text_content must be provided when audio_path is provided.")
     if not os.path.exists(f"./vectorstore/{index_path}"):
         if image_path:
             index = faiss.IndexFlatL2(512)
@@ -42,6 +45,15 @@ def update_vectordb(index_path: str, embedding: torch.Tensor, image_path: str = 
             new_entry_df = pd.DataFrame({"path": image_path, "index": len(df)}, index=[0])
             df = pd.concat([df, new_entry_df], ignore_index=True)
             df.to_csv("./vectorstore/image_data.csv", index=False)
+    elif audio_path:
+        if not os.path.exists("./vectorstore/audio_data.csv"):
+            df = pd.DataFrame([{"path": audio_path, "content": text_content, "index": 0}]).reset_index(drop=True)
+            df.to_csv("./vectorstore/audio_data.csv", index=False)
+        else:
+            df = pd.read_csv("./vectorstore/audio_data.csv").reset_index(drop=True)
+            new_entry_df = pd.DataFrame({"path": audio_path, "content": text_content, "index": len(df)}, index=[0])
+            df = pd.concat([df, new_entry_df], ignore_index=True)
+            df.to_csv("./vectorstore/audio_data.csv", index=False)
     elif text_content:
         if not os.path.exists("./vectorstore/text_data.csv"):
             df = pd.DataFrame([{"content": text_content, "index": 0}]).reset_index(drop=True)
@@ -119,6 +131,41 @@ def add_pdf_to_index(pdf, clip_model: clip.model.CLIP, preprocess, text_embeddin
         percent_complete = ((page_num + 1) / len(pdf_reader.pages))
         progress_bar.progress(percent_complete, f"Processing Page {page_num + 1}/{len(pdf_reader.pages)}")
     return pdf_pages_data
+
+
+def add_audio_to_index(audio, whisper_model: whisper.Whisper, text_embedding_model: SentenceTransformer):
+    if not os.path.exists("./vectorstore/"):
+        os.makedirs("./vectorstore")
+    if not os.path.exists("./audio"):
+        os.makedirs("./audio")
+    if hasattr(audio, "name"):
+        audio_name = audio.name
+    else:
+        audio_name = f"{time.time()}.wav"
+    audio_name = audio_name.replace(" ", "_")
+    with open(f"./audio/{audio_name}", "wb") as f:
+        try:
+            f.write(audio.read())
+        except:
+            if hasattr(audio, "data"):
+                audio = io.BytesIO(audio.data)
+            else:
+                audio = io.BytesIO(audio)
+            f.write(audio.read())
+    audio_transcript: str = whisper_model.transcribe(f"./audio/{audio_name}")["text"]
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    chunks = text_splitter.split_text(audio_transcript)
+    text_embeddings = text_embedding_model.encode(chunks)
+    for i, chunk in enumerate(chunks):
+        update_vectordb(index_path="audio_index.index", embedding=text_embeddings[i], text_content=chunk, audio_path=f"./audio/{audio_name}")
+    return audio_transcript
+
 
 def search_image_index_with_image(image_features, index: faiss.IndexFlatL2, clip_model: clip.model.CLIP, k: int = 3):
     with torch.no_grad():
